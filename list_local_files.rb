6f969bb58d55dad1ee183550a9d7df5da8e4f37c
Giancarlo Franchini <gfranchini88@gmail.com>
2016-05-17 00:28:00 -0400

    added ability to traverse filesystem for file on windows or mac

--- list_local_files.rb
+++ list_local_files.rb
@@ -1,3 +1,12 @@
-File.open("files.txt").each do |line|
+#prints current directory
+# puts Dir.pwd
+
+#on ubuntu/mac
+# File.open("files.txt").each do |line|
+#   puts line
+# end
+
+#on windows
+File.open("C:\\Users\\compuser\\Desktop\\test.txt").each do |line|
   puts line
 end
