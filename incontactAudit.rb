859a96084b3431b21aa8c50026b26d70ba63984d
Giancarlo Franchini <gfranchini@greathealthworks.com>
2016-05-28 14:34:24 -0400

    changed instance variables

--- incontactAudit.rb
+++ incontactAudit.rb
@@ -4,15 +4,7 @@ require 'fileutils'
 
 user = ENV['USERNAME']
 incontact = []
-Net::SFTP.start(HOST, USERNAME, :password => PASSWORD) do |sftp|
-  sftp.dir.foreach("/") do |entry|
-	incontact << entry.name
-	File.open("incontact_files.csv", "w+") do |f|
-		incontact.each {|x| f.puts(x)}
-	end
-  end
-end
-FileUtils.mv("incontact_files.csv","C:\\Users\\#{user}\\Desktop\\")
+FileUtils.mkdir_p('C:\\Users\\#{user}\\Desktop\\AuditFiles')
 
 local = Array.new
 Dir.chdir(DRIVE)  
@@ -23,15 +15,26 @@ Dir['*'].each do |x|
 	end
 end
 FileUtils.mv("local_files.csv","C:\\Users\\#{user}\\Desktop\\")
- 
-audit = Array.new
-audit = incontact - local
+
+
+Net::SFTP.start(HOST, USERNAME, :password => PASSWORD) do |sftp|
+  sftp.dir.foreach("/") do |entry|
+	incontact << entry.name
+	File.open("incontact_files.csv", "w+") do |f|
+		incontact.each {|x| f.puts(x)}
+	end
+  end
+  
+  audit = Array.new
+  @audit = incontact - local
+  
+end
+FileUtils.mv("incontact_files.csv","C:\\Users\\#{user}\\Desktop\\")
 
 File.open("audit.csv", "w+") do |f|
-	audit.each {|x| f.puts(x)}
+	@audit.each {|x| f.puts(x)}
 end
+
 FileUtils.mv("audit.csv","C:\\Users\\#{user}\\Desktop\\")
 
-p incontact.count
-p local.count
-p audit.count
+#sftp.download!("/path/to/remote", "/path/to/local")
